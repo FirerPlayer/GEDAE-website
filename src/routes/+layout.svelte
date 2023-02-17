e0db12868f890c9a5d103b7610446216ed130207
Micael Fernandes <micaelnandes21@hotmail.com>
2023-02-17 16:02:30 -0300

    udpate mobile view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,14 +6,14 @@
 	import MobileMenuDropdown from '$lib/components/MobileMenuDropdown.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import { fly } from 'svelte/transition';
-	import { quintOut } from 'svelte/easing';
+	import { elasticInOut, quintOut } from 'svelte/easing';
 	import { browser } from '$app/environment';
 	export let title = `GEDAE | ${$page.route.id === '/' ? 'Home' : $page.route.id}`;
 	const dropEstrutura = {
 		name: 'Estrutura',
 		items: [
 			{
-				name: 'Loboratórios',
+				name: 'Laboratórios',
 				link: '#'
 			},
 			{
@@ -59,6 +59,18 @@
 	if (browser) {
 		isMobile = window.matchMedia('(max-width: 768px)').matches;
 	}
+	let isDropdownOpen: boolean = false;
+	const customTransition = () => {
+		return {
+			css: (t: number) => {
+				return `
+        transform: height;
+        `;
+			},
+			easing: quintOut,
+			duration: 200
+		};
+	};
 </script>
 
 <svelte:head>
@@ -73,47 +85,48 @@
 
 <!-- svelte-ignore a11y-missing-attribute -->
 {#if isMobile}
-	<div class="drawer">
-		<input id="drawer1" type="checkbox" class="drawer-toggle" />
-		<div class="drawer-content">
-			<div class="navbar bg-base-200 border-b-2 border-base-300 p-0 min-h-fit">
-				<div class="navbar-start">
-					<label for="drawer1" class="btn btn-square btn-ghost">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							class="inline-block w-6 h-6 stroke-current"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M4 6h16M4 12h16M4 18h16"
-							/></svg
-						>
-					</label>
-				</div>
-				<div class="navbar-center">
-					<a href="/"><img src={logo} alt="" class="h-16" /></a>
-				</div>
-			</div>
-			<slot />
-			<Footer />
+	<div class="navbar bg-base-200 border-b-2 border-base-300 p-0 min-h-fit z-10">
+		<div class="navbar-start">
+			<a href="/"><img src={logo} alt="" class="h-16" /></a>
 		</div>
-		<div class="drawer-side">
-			<label for="drawer1" class="drawer-overlay" />
-			<ul class="menu p-2 w-72 bg-base-200">
-				{#each menuItems as item}
-					<li class="">
-						<a class="h-full" href={item.link}>{item.name}</a>
-					</li>
-				{/each}
-				<MobileMenuDropdown menuItems={dropEstrutura} />
-				<MobileMenuDropdown menuItems={dropProjetos} />
-				<!-- <div class="bg-black self-center place-self-start"></div> -->
-			</ul>
+		<div class="navbar-end">
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div
+				class="btn btn-square btn-ghost"
+				on:click={() => {
+					isDropdownOpen = !isDropdownOpen;
+				}}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block w-6 h-6 stroke-current"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					/></svg
+				>
+			</div>
 		</div>
 	</div>
+
+	{#if isDropdownOpen}
+		<ul class="menu p-2 w-full bg-base-200 -z-10">
+			{#each menuItems as item}
+				<li class="">
+					<a class="h-full" href={item.link}>{item.name}</a>
+				</li>
+			{/each}
+			<MobileMenuDropdown menuItems={dropEstrutura} />
+			<MobileMenuDropdown menuItems={dropProjetos} />
+			<!-- <div class="bg-black self-center place-self-start"></div> -->
+		</ul>
+	{/if}
+	<slot />
+	<Footer />
 {:else}
 	<div class="flex flex-col  items-center min-w-full bg-base-300">
 		<div class="max-w-[1440px] bg-base-200">
